d8d14b027b14d03a5d09a21745ad0fe4db8466ae
PongsathornXCody <pongsathorn.T0511@gmail.com>
2025-11-04 17:52:00 +0700

    อัปเดตโค้ดล่าสุดก่อนพัก

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
-// https://vite.dev/config/
+
 export default defineConfig({
   base: '/my-finalproject-maikluea/',
-  plugins: [react(),
-  VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-    manifest:
-    {
-      name: "my-finalproject-maikluea",
-      short_name: 'MaiKluea',
-      description: 'แอบพลิเคชัน สำหรับ คำนวณเพชรเปิดกาชา',
-      theme_color: '#ffffff',
-      icons:
-      [
-        {
-          src:'pwa-192x192.png',
-          sizes: '256x256',
-          type: 'image/png',
-        },
-        {
-          src:'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-        {
-          src:'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable'
-        },
-      ]
-    }
-  })
-]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'masked-icon.svg'
+      ],
+      manifest: {
+        name: "My Final Project - MaiKluea",
+        short_name: "MaiKluea",
+        description: "แอปพลิเคชันสำหรับคำนวณเพชรเปิดกาชา",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "./",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
+      }
+    })
+  ]
 })
